a842e5ae984ede77ae06c1e044597a80210d319b
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-04-19 22:54:22 +0900

    chore: param test

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
