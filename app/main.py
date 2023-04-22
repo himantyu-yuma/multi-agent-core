99a70e7e710a45582d6597cdf4feaf1e367a57f6
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-04-23 03:43:53 +0900

    update: 終了時にmongoとの接続を切断

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
-# NOTE: 型定義は外部から呼び出す
-from models.speaker import Speacker
 from models.chat_message import ChatMessage
 
+# NOTE: 型定義は外部から呼び出す
+from models.speaker import Speacker
+
 app = FastAPI()
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    # FIXME: mongo client をcloseする処理
+    pass
 
 
 # TODO: api dir以下に移動する？
