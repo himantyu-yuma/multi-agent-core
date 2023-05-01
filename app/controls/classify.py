afab7b0eb9ec1125c2be52edf4582a2cb2b70485
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-02 00:38:16 +0900

    update: classify function and template

--- app/controls/classify.py
+++ app/controls/classify.py
@@ -1,3 +1,5 @@
+import json
+
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
@@ -13,8 +15,10 @@ def classify(sentence: str, speaker: str):
     )
     chain = LLMChain(llm=chatGpt, prompt=prompt)
     result = chain.run({"sentence": sentence, "speaker": speaker})
-    return result
+    print(result)
+    address = json.loads(result)["address"]
+    return address
 
 
 if __name__ == "__main__":
-    print(classify("こんにちは！サイお姉ちゃんは今日何をしていたの？", "NOI"))
+    print(classify("自己紹介お願いします", "User"))
--- app/models/chat_message.py
+++ app/models/chat_message.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 
 class ChatMessage(BaseModel):
     content: str
+
+
+class ChatMessageWithSpeaker(BaseModel):
+    speaker: str
+    content: str
