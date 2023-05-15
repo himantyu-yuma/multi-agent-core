90674cade4029e2166439702ef13661bdd0f7f39
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-16 02:14:26 +0900

    update: delete unnecessary lines

--- app/controls/script.py
+++ app/controls/script.py
@@ -1,9 +1,7 @@
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
 
 from controls.llm import chatGpt
-from domain.memory.mongoDB import MongoChatMessageHistory
 from settings.get_template import get_template
 
 
@@ -13,22 +11,13 @@ def create_script(topic: str):
     """
     template = get_template("CreateScript")
 
-    mongo_uri = "mongodb://root:root@mongo:27017/messages?authSource=admin"
-    history = MongoChatMessageHistory(
-        session_id=topic, connection_string=mongo_uri, collection_name="script"
-    )
-    memory = ConversationBufferMemory(
-        chat_memory=history, memory_key="chat_history"
-    )
-
     prompt = PromptTemplate(
         template=template,
         input_variables=["topic"],
     )
 
-    chain = LLMChain(llm=chatGpt, prompt=prompt, memory=memory)
+    chain = LLMChain(llm=chatGpt, prompt=prompt)
     result = chain.run({"topic": topic})
-    history.client.close()
 
     return result
 
