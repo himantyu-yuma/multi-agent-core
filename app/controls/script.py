c02d21c3ed5c6c1b2f6a2b41c777b1a772832aa2
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-16 01:31:02 +0900

    add: 台本作成ができるように

---
+++ app/controls/script.py
@@ -0,0 +1,38 @@
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+from controls.llm import chatGpt
+from domain.memory.mongoDB import MongoChatMessageHistory
+from settings.get_template import get_template
+
+
+def create_script(topic: str):
+    """
+    指定されたトピックについて、三姉妹の台本を作る
+    """
+    template = get_template("CreateScript")
+
+    mongo_uri = "mongodb://root:root@mongo:27017/messages?authSource=admin"
+    history = MongoChatMessageHistory(
+        session_id=topic, connection_string=mongo_uri, collection_name="script"
+    )
+    memory = ConversationBufferMemory(
+        chat_memory=history, memory_key="chat_history"
+    )
+
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["topic"],
+    )
+
+    chain = LLMChain(llm=chatGpt, prompt=prompt, memory=memory)
+    result = chain.run({"topic": topic})
+    history.client.close()
+
+    return result
+
+
+if __name__ == "__main__":
+    sample = create_script("学食をこれから食べる予定であり、チキンタツタ丼がおすすめである")
+    print(sample)
