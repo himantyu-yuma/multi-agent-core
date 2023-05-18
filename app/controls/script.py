e71244f6935be3c94873d12cb4e45b7fa0519ec9
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-19 01:48:47 +0900

    update: 台本作成用エンドポイント追加

--- app/controls/script.py
+++ app/controls/script.py
@@ -1,13 +1,25 @@
+import datetime
+import re
+
+from bson import ObjectId
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 
 from controls.llm import chatGpt
+from repository.database.mongoDB import MongoDB
 from settings.get_template import get_template
 
 
-def create_script(topic: str):
+def create_script(
+    topic: str, instruction: str, published_at: datetime.datetime
+):
     """
     指定されたトピックについて、三姉妹の台本を作る
+    Args:
+        topic: str : トピック(DB保存用)
+        instruction: str : 生成させる際の命令
+    Returns:
+        生成された台本のmongo上のID
     """
     template = get_template("CreateScript")
 
@@ -17,8 +29,32 @@ def create_script(topic: str):
     )
 
     chain = LLMChain(llm=chatGpt, prompt=prompt)
-    result = chain.run({"topic": topic})
+    result = chain.run({"topic": instruction})
 
+    lines = result.split("\n")
+    scripts = []
+    line_order = 0
+    for line in lines:
+        if line == "":
+            continue
+        (speaker, quote) = re.split("[:：]", re.sub(" 　", "", line))
+        scripts.append(
+            {"order": line_order, "speaker": speaker, "quote": quote}
+        )
+        line_order += 1
+
+    data = {
+        "topic": topic,
+        "scripts": scripts,
+        "published_at": published_at,
+        "created_at": datetime.datetime.now(),
+    }
+
+    client = MongoDB(collection_name="scripts")
+    scripts_id = client.insert_one(data).inserted_id
+    client.close_connection()
+
+    return str(scripts_id)
     return result
 
 
