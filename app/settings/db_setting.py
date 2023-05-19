f834dc70ef3a8ff29d6eae60a59bd7c6a8830dfa
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-19 16:50:46 +0900

    add: db設定用ファイル作成

---
+++ app/settings/db_setting.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+# FIXME: 環境変数から取得したい
+CONNECTION_STRING = "mongodb://root:root@mongo:27017/messages?authSource=admin"
+
+client = MongoClient(CONNECTION_STRING)
+db = client.get_default_database()
+user_collection = db["user_responses"]
+
+# script_idとbreak_pointの組み合わせに一意のインデックスを作成
+user_collection.create_index(
+    [("script_id", 1), ("break_point", 1)], unique=True
+)
