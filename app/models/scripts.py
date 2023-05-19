8ac6bd2522d0c5907940b1abef8249a160129a85
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-05-19 17:13:01 +0900

    update: example追加

--- app/models/scripts.py
+++ app/models/scripts.py
@@ -7,3 +7,12 @@ class CreateScriptRequest(BaseModel):
     topic: str
     instraction: str
     published_at: datetime.datetime
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "topic": "学食",
+                "instraction": "三姉妹は学食をこれから食べる予定であり、チキンタツタ丼がおすすめであることについて話している",
+                "published_at": datetime.datetime(2023, 5, 19, 12, 0, 0),
+            }
+        }
--- app/models/user_response.py
+++ app/models/user_response.py
@@ -5,6 +5,14 @@ class CreateUserResponseRequest(BaseModel):
     script_id: str
     break_point: int
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "script_id": "64661c7cab6e66270e943696",
+                "break_point": 3,
+            }
+        }
+
 
 class GetUserResponsesRequest(BaseModel):
     script_id: str
